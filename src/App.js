06611b17de75019e1118bc55652dd57c78fba1a4
Divya <divyasaroj29@gmail.com>
2023-07-13 13:32:57 +0530

    add app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
 import "./App.css";
-
+import PopupWizard from "./component/popupwizard/PopupWizard";
+import ContextMain from "./component/Context/ContextMain";
+import Button from "./component/ButtonComponent/Button";
 function App() {
-  return <div className="App"></div>;
+  const [showform, setShowform] = useState(false);
+
+  const closeForm = () => {
+    setShowform(false);
+  };
+  return (
+    <>
+      <div>
+        <div className="home-container">
+          <Button
+            onClick={() => {
+              setShowform(true);
+            }}
+            name="PopupWizard"
+          />
+          <ContextMain />
+        </div>
+      </div>
+      {showform && <PopupWizard closeForm={closeForm} />}
+    </>
+  );
 }
 
 export default App;
